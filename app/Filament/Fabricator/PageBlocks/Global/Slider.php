ddb828f38bb5cc116b2c26f8bd6fee7f07babd0c
Ali Tezcan <alitezcan147@icloud.com>
2024-09-23 12:17:24 +0300

    V 1.0.2 add Corporate Slider

---
+++ app/Filament/Fabricator/PageBlocks/Global/Slider.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Filament\Fabricator\PageBlocks\Global;
+
+use Filament\Forms\Components\Builder\Block;
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\Repeater;
+use Filament\Forms\Components\TextInput;
+use Z3d0X\FilamentFabricator\PageBlocks\PageBlock;
+
+class Slider extends PageBlock
+{
+    public static function getBlockSchema(): Block
+    {
+        return Block::make('global.slider')
+            ->schema([
+                Repeater::make('sliders')
+                    ->columns([
+                        'sm' => 2,
+                        'xl' => 6,
+                        '2xl' => 6,
+                    ])
+                    ->schema([
+                        FileUpload::make('image')
+                            ->columnSpan([
+                                'sm' => 2,
+                                'xl' => 6,
+                                '2xl' => 6,
+                            ])
+                            ->label('Görsel')
+                            ->image()
+                            ->maxSize(150000)
+                            ->disk('public')
+                            ->required()
+                            ->directory('img')
+                            ->optimize('webp'),
+                        TextInput::make('subtitle')->columnSpan([
+                            'sm' => 2,
+                            'xl' => 6,
+                            '2xl' => 6,
+                        ])->label('Kısa Başlık')->live(onBlur: true),
+                        TextInput::make('title')->columnSpan([
+                            'sm' => 2,
+                            'xl' => 6,
+                            '2xl' => 6,
+                        ])->label('Başlık'),
+                        TextInput::make('description')->columnSpan([
+                            'sm' => 2,
+                            'xl' => 6,
+                            '2xl' => 6,
+                        ])->label('Açıklama'),
+                        TextInput::make('url')->columnSpan([
+                            'sm' => 2,
+                            'xl' => 3,
+                            '2xl' => 3,
+                        ])->label('URL'),
+                        TextInput::make('button_name')->columnSpan([
+                            'sm' => 2,
+                            'xl' => 3,
+                            '2xl' => 3,
+                        ])->label('Buton Metni'),
+                    ])
+                    ->collapsed()
+                    ->itemLabel(fn (array $state): ?string => $state['subtitle'] ?? null)
+                    ->label('Slider'),
+            ])->label('Hero Slider')->visible(fn ($get) => $get('../layout') == 'corporate');
+    }
+
+    public static function mutateData(array $data): array
+    {
+        return $data;
+    }
+}
---
+++ resources/views/components/filament-fabricator/page-blocks/global/slider.blade.php
@@ -0,0 +1,77 @@
+@aware(['page'])
+<section class="banner banner--main window--inner-height">
+    <div class="container-full">
+        <div class="swiper"
+             data-swiper="{ autoplay: { delay: 4000 }, speed: 1200, parallax: true, video: true }">
+            <div class="swiper-container">
+                <div class="swiper-wrapper">
+                    @foreach($sliders as $slide)
+                        <div class="swiper-slide" data-swiper-bullet-name="{!! $slide['subtitle'] !!}"
+                        >
+                            <figure class="banner-image background--fixed loader--loaded"
+                                    src="{{ asset('front/images/empty.png')}}"
+                                    data-src="{{ \Illuminate\Support\Facades\Storage::url($slide['image']) }}"
+                                    data-loader="{ bg: false, over: true }"
+                                    data-swiper-parallax-opacity="0.4"
+                                    style="background-image: url({{ \Illuminate\Support\Facades\Storage::url($slide['image']) }});">
+                            </figure>
+                            <div class="banner-content">
+                                <div
+                                    class="container d-flex align-items-center justify-content-center text-center">
+                                    <article class="content-body h-auto">
+                                        <h1 class="content-title h1" data-swiper-parallax="-350"
+                                            data-swiper-parallax-opacity="0">
+                                            {!! $slide['title'] !!}</h1>
+                                        <p class="content-description" data-swiper-parallax="-250"
+                                           data-swiper-parallax-opacity="0">
+                                            {!! $slide['description'] !!}</p>
+                                        <div data-swiper-parallax="-150" data-swiper-parallax-opacity="0"
+                                             style="opacity: 1; transform: translate3d(0px, 0px, 0px);"><a
+                                                href="{{ $slide['url'] }}"
+                                                target="_blank"
+                                                class="btn btn-default btn-light swipebox swipebox-video" rel="34"
+                                                title="{{ $slide['button_name'] }}">{{ $slide['button_name'] }}</a></div>
+                                    </article>
+                                </div>
+                            </div>
+                        </div>
+                    @endforeach
+                </div>
+                <span class="swiper-notification" aria-live="assertive" aria-atomic="true"></span>
+            </div>
+        </div>
+        <div class="copyright d-none d-sm-block">
+            <p>©2019, Polat Enerji. Tüm hakları saklıdır.</p>
+            <ul>
+                <li><a href="/politikalar-ve-belgeler/" title="Politikalar ve Belgeler"
+                       class="active">Politikalar ve Belgeler</a></li>
+                <li><a href="/cerez-politikasi/" title="Çerez Politikası" class="active">Çerez
+                        Politikası</a></li>
+            </ul>
+        </div>
+        <div class="scroll-down js-scroll-down" title="Aşağıya Kaydır">
+            <svg version="1.1" id="Layer_1"
+                 xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px"
+                 y="0px" viewBox="0 0 117 267" style="enable-background:new 0 0 117 267;"
+                 xml:space="preserve" class="svg replaced-svg" height="26">
+                            <g>
+                                <polygon points="117,109 64,109 113,0 56,0 0,136 54,136 10,267  "></polygon>
+                            </g>
+                        </svg>
+        </div>
+        <div class="social-links">
+            <ul>
+                <li><a href="https://www.facebook.com/polatenerji" target="_blank" title="Facebook"
+                       rel="me"><i class="icon-facebook-1"></i></a></li>
+                <li><a href="https://www.instagram.com/polatenerji/" target="_blank" title="Instagram"
+                       rel="me"><i class="icon-instagram-1"></i></a></li>
+                <li><a href="https://www.linkedin.com/company/polat-enerji-san.-ve-tic.-a.s/about/"
+                       target="_blank" title="Linked In" rel="me"><i class="icon-linkedin-1"></i></a></li>
+                <li><a href="https://www.youtube.com/@polat.enerji" target="_blank" title="Youtube"
+                       rel="me"><i class="icon-youtube-play"></i></a></li>
+            </ul>
+        </div>
+    </div>
+</section>
+
+<div class="pb-4 pb-sm-6 pb-xl-8"></div>
