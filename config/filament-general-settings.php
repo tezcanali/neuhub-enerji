ef8f757ecfbd845ab0335a3042aaf28447a24d10
Ali Tezcan <alitezcan147@icloud.com>
2024-10-21 14:19:07 +0300

    V 1.0.6 footer and header update

--- config/filament-general-settings.php
+++ config/filament-general-settings.php
@@ -6,7 +6,7 @@ return [
     'show_application_tab' => false,
     'show_analytics_tab' => false,
     'show_seo_tab' => false,
-    'show_email_tab' => false,
+    'show_email_tab' => true,
     'show_social_networks_tab' => true,
     'expiration_cache_config_time' => 60,
     'show_custom_tabs'=> true,
