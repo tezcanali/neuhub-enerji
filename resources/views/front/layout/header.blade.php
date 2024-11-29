8bacc02515346ec85d1c38ad66386c2107a69588
Ali Tezcan <alitezcan147@icloud.com>
2024-11-29 09:14:08 +0300

    V 1.1.4

--- resources/views/front/layout/header.blade.php
+++ resources/views/front/layout/header.blade.php
@@ -17,6 +17,7 @@
                         <li><a href="{{ $locale == 'en' ? '/en/power-plants' : '/santrallerimiz' }}" class="active"
                                title="@lang('front.header.powerplants')">@lang('front.header.powerplants')</a></li>
                         @if($locale == 'tr')
+                            <li><a href="/surdurulebilirlik" class="active" title="Sürdürülebilirlik">Sürdürebilirlik</a></li>
                             <li><a href="/haberler" class="active" title="@lang('front.header.news')">@lang('front.header.news')</a></li>
                         @endif
                         <li><a href="{{ $locale == 'en' ? '/en/contact' : '/iletisim' }}" class="active" title="@lang('front.header.contact')">@lang('front.header.contact')</a></li>
